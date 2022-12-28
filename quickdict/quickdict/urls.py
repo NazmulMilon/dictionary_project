2e7ec269e60e3c8d5de1cd294e4bb165209e7f52
nazmulmilon <milonsarker73@gmail.com>
2022-12-28 09:19:40 +0600

    users app created

--- quickdict/quickdict/urls.py
+++ quickdict/quickdict/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_create'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # path('', include('dictionary.urls')),
 ]
