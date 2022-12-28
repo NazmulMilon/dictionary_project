31d7bda704d0cdc323d8e86c7ecd2886a51c39a5
nazmulmilon <milonsarker73@gmail.com>
2022-12-28 12:20:16 +0600

    updated code

--- quickdict/users/serializers.py
+++ quickdict/users/serializers.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.models import User
 from .models import UserProfile
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 
 class UserProfileSerializer(ModelSerializer):
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    username = serializers.CharField(required=True)
+    email = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
     class Meta:
         model = UserProfile
-        fields = '__all__'
+        exclude = ['user']
--- quickdict/users/views.py
+++ quickdict/users/views.py
@@ -6,21 +6,31 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
+from django.db import transaction
+
 # Create your views here.
 
 
 class UserProfileCreateView(CreateAPIView):
     serializer_class = UserProfileSerializer
 
+    @transaction.atomic
     def post(self, request, *args, **kwargs):
         data = request.data
-        serializer = self.serializer_class(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            response = {
-                "details": "User created Successfully",
-                "data": serializer.data
-            }
-            return Response(data=response, status=status.HTTP_201_CREATED)
+        first_name = data.get('first_name', None)
+        last_name = data.get('last_name', None)
+        username = data.get('username', None)
+        email = data.get('email', None)
+        phone_number = data.get('phone_number', None)
+        date_of_birth = data.get('date_of_birth', None)
+        password = data.get('password', None)
 
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if User.objects.filter(username=username).exists():
+            return Response(data={'details': 'User name already exists. '}, status=status.HTTP_406_NOT_ACCEPTABLE)
+        user = User(first_name=first_name, last_name=last_name, username=username, email=email,
+                    password=password)
+        user.save()
+
+        user_profile = UserProfile(phone_number=phone_number, date_of_birth=date_of_birth, user=user)
+        user_profile.save()
+        return Response(data={'details': 'User Created'}, status=status.HTTP_201_CREATED)
